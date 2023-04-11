b935621da4ccaba09d6d88f02e48a6877fa347e6
pallav saikia <pallav.saikia@styldod.com>
2023-04-11 19:35:45 +0530

    added mongodb to cluster

--- authentication/src/app.ts
+++ authentication/src/app.ts
@@ -1,6 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
+import mongoose from 'mongoose';
 
+const mongoDbStart = async () => {
+    try {
+        await mongoose.connect("mongodb://authentication-mongo-srv:27017/auth")
+        console.log('connected to db')
+    } catch (e) {
+        console.log(e)
+    }
+
+}
 export function app() {
     const app = express();
     app.use(morgan('dev'))
@@ -8,6 +18,6 @@ export function app() {
     app.get('/', (req, res) => {
         res.send('hello world')
     })
-    
+    mongoDbStart()
     return app
 }
